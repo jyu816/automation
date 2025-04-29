d4a57f3e4b372f0d399ed8c908b872eafc0d9ce0
jyu816 <taoyuca@gmail.com>
2025-04-28 20:25:16 -0700

    Update backup_pan.py

--- backup/backup_pan.py
+++ backup/backup_pan.py
@@ -17,11 +17,17 @@ def backup_pan(device,backup_path,logging):
         pass
     else:
         timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        backup = f"{backup_path}/running-config_{device['host']}_{timestamp}.xml"
-        # export configuration from PAN
-        url_api = f"https://{device['host']}/api/?type=export&category=configuration&key={apiKey}"
-        resp = requests.request("GET", url_api, verify=False)
-        with open(backup, 'wb') as f:
-            f.write(resp.content)
-        with open(logging, 'a') as f:
-            f.write(f"---\n\nSuccessfully run configuration backup on {device['host']}\n\n")
+        backup = f"{backup_path}/device-state_{device['host']}_{timestamp}.tgz"
+        # export device-state from PAN
+        url_api = f"https://{device['host']}/api/?type=export&category=device-state&key={apiKey}"
+        resp = requests.request("GET", url_api, stream=True, verify=False)
+        if resp.status_code == 200:
+            with open(backup, 'wb') as f:
+                for chunk in resp.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+            with open(logging, 'a') as f:
+                f.write(f"---\n\nSuccessfully run configuration backup on {device['host']}\n\n")
+        else:
+            with open(logging, 'a') as f:
+                f.write(f"---\n\nFailed to run configuration backup on {device['host']}\n\n")
